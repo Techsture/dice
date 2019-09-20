96691883b82f3d5e4797621b741764f1dcbe799f
Michael Andrews <mandrews4@apple.com>
2019-09-20 14:06:46 -0700

    It's broken, but whatever.

--- dice.py
+++ dice.py
@@ -3,33 +3,42 @@
 # How many tries will it take to get `1, 2, 3, 4, 5, 6` in a row?
 # Maths says 6^6 (46,656) tries.
 
-# TODO: Set this up so it continues to execute, and calculate the running average.
+# TODO: Something is wrong with him I'm calculating the running average.  The average seems to be approaching zero.
+# I'm doing something fundamentally wrong.
 
+from math import ceil
 from random import randint
 
 
 def roll():
-    die_roll = randint(1, 6)
-    print(die_roll)
-    return die_roll
+  die_roll = randint(1, 6)
+  return die_roll
 
 
 def main():
-    success = False
-    counter = 0
-    while success is False:
-        counter += 1
-        print("\n")
-        if roll() == 1:
-            if roll() == 2:
-                if roll() == 3:
-                    if roll() == 4:
-                        if roll() == 5:
-                            if roll() == 6:
-                                success = True
-    print("Success!  It took {0} tries.".format(counter))
-    exit()
+  running_average = 0
+  success_counter = 0
+  tries_counter = 0
+  while True:
+    tries_counter += 1
+    if roll() == 1:
+      if roll() == 2:
+        if roll() == 3:
+          if roll() == 4:
+            if roll() == 5:
+              if roll() == 6:
+                success_counter += 1
+                if running_average > 0:
+                  print("({} + {}) / {}".format(running_average, tries_counter, success_counter))
+                  running_average = ceil((running_average + tries_counter) / success_counter)
+                  print("Running average is {}.".format(running_average))
+                else:
+                  print("({} + {}) / {}".format(running_average, tries_counter, success_counter))
+                  running_average = ceil(tries_counter / success_counter)
+                  print("Running average is {}.".format(running_average))
+                tries_counter = 0
+  exit()
 
 
 if __name__ == '__main__':
-    main()
+  main()
